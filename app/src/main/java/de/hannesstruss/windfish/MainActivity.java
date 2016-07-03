d70b18f176500e94b9c12e2ae27d8838bc43449d
Hannes Struß <x@hannesstruss.de>
2016-07-03 03:30:02 +0200

    Add help link

--- app/src/main/java/de/hannesstruss/windfish/MainActivity.java
+++ app/src/main/java/de/hannesstruss/windfish/MainActivity.java
@@ -1,10 +1,14 @@
 package de.hannesstruss.windfish;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+  private static final String HELP_URL = "https://youtu.be/AuuIB4cT2SA";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,13 @@ public class MainActivity extends Activity {
 
     String hello = "\uD83D\uDC33";
     ((TextView) findViewById(R.id.textview)).setText(hello);
+
+    findViewById(R.id.btn_help_me).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(HELP_URL));
+        startActivity(intent);
+      }
+    });
   }
 }
