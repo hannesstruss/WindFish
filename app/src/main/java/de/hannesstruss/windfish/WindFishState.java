3d881bff059c8014dc359b559745d4536c1a8537
Hannes Struß <x@hannesstruss.de>
2016-12-03 18:09:10 +0100

    Introduce Mode enum

--- app/src/main/java/de/hannesstruss/windfish/WindFishState.java
+++ app/src/main/java/de/hannesstruss/windfish/WindFishState.java
@@ -8,25 +8,48 @@ import com.f2prateek.rx.preferences.Preference;
 import com.f2prateek.rx.preferences.RxSharedPreferences;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 class WindFishState {
-  private static final String KEY_ENABLED = "enabled";
+  private enum Mode {
+    ALWAYS_OFF, /*ON_WHEN_ON_POWER,*/ ALWAYS_ON
+  }
 
-  private final Preference<Boolean> enabled;
+  private static final String KEY_MODE = "mode";
+
+  private static final Func1<String, Mode> TO_MODE = Mode::valueOf;
+
+  private final Preference<String> mode;
 
   WindFishState(Application context) {
     SharedPreferences sharedPreferences =
         context.getSharedPreferences("windfish_state.prefs", Context.MODE_PRIVATE);
     RxSharedPreferences rxPrefs = RxSharedPreferences.create(sharedPreferences);
-    enabled = rxPrefs.getBoolean(KEY_ENABLED, false);
+    mode = rxPrefs.getString(KEY_MODE, Mode.ALWAYS_OFF.name());
   }
 
   Observable<Boolean> isEnabled() {
-    return enabled.asObservable();
+    return mode.asObservable()
+        .map(Mode::valueOf)
+        .map(mode -> mode == Mode.ALWAYS_ON);
   }
 
   void toggle() {
-    //noinspection ConstantConditions
-    enabled.set(!enabled.get());
+    mode.asObservable()
+        .first()
+        .map(TO_MODE)
+        .subscribe(mode -> {
+          Mode nextMode;
+          if (mode == Mode.ALWAYS_ON) {
+            nextMode = Mode.ALWAYS_OFF;
+          } else {
+            nextMode = Mode.ALWAYS_ON;
+          }
+          setMode(nextMode);
+        });
+  }
+
+  private void setMode(Mode nextMode) {
+    mode.set(nextMode.name());
   }
 }
