42f18f2445bfe268e829c91cfcf4f4cdd54763b4
Hannes Struß <x@hannesstruss.de>
2016-12-03 16:22:30 +0100

    Tighten visibilities

--- app/src/main/java/de/hannesstruss/windfish/WindFishState.java
+++ app/src/main/java/de/hannesstruss/windfish/WindFishState.java
@@ -7,22 +7,22 @@ import android.content.SharedPreferences;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class WindFishState {
+class WindFishState {
   private static final String KEY_ENABLED = "enabled";
 
   private final SharedPreferences sharedPreferences;
   private final Set<Listener> listeners;
 
-  public WindFishState(Application context) {
+  WindFishState(Application context) {
     this.sharedPreferences = context.getSharedPreferences("windfish_state.prefs", Context.MODE_PRIVATE);
     listeners = new LinkedHashSet<>();
   }
 
-  public boolean isEnabled() {
+  boolean isEnabled() {
     return sharedPreferences.getBoolean(KEY_ENABLED, false);
   }
 
-  public void setEnabled(boolean enabled) {
+  private void setEnabled(boolean enabled) {
     sharedPreferences.edit().putBoolean(KEY_ENABLED, enabled).apply();
     
     for (Listener listener : listeners) {
@@ -30,15 +30,15 @@ public class WindFishState {
     }
   }
   
-  public void addListener(Listener listener) {
+  void addListener(Listener listener) {
     listeners.add(listener);
   }
 
-  public void toggle() {
+  void toggle() {
     setEnabled(!isEnabled());
   }
 
-  public void removeListener(Listener listener) {
+  void removeListener(Listener listener) {
     listeners.remove(listener);
   }
 
